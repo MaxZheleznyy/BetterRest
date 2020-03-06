2afdf2051845502803d95d793b788d6fca30723e
Max <maxzheleznyy@gmail.com>
2020-03-05 23:56:39 -0800

    Sections instead of VStacks + go to bed calculation on the fly

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -13,9 +13,41 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 0
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("When do you want to wake up?")) {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                    .datePickerStyle(WheelDatePickerStyle())
+                }
+
+                Section(header: Text("Your desired amount of sleep")) {
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                
+                Section(header: Text("Your daily coffee intake")) {
+                    Picker("Select number of cups", selection: $coffeeAmount) {
+                        ForEach(0...20, id: \.self) { cups in
+                            (cups == 1) ? Text("1 cup") : Text("\(cups) cups")
+                        }
+                    }
+                    .labelsHidden()
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(height: 150)
+                    .clipped()
+                }
+                
+                Section(header: Text("Go to bed before").font(.headline)) {
+                    Text("\(calculateBedtime)")
+                    .font(Font.title.monospacedDigit())
+                }
+            }
+            .navigationBarTitle("BetterRest")
+        }
+    }
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -24,53 +56,7 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date()
     }
     
-    var body: some View {
-        NavigationView {
-            Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                    .datePickerStyle(WheelDatePickerStyle())
-                }
-
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-
-                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                        Text("\(sleepAmount, specifier: "%g") hours")
-                    }
-                }
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-
-                    Stepper(value: $coffeeAmount, in: 0...20) {
-                        if coffeeAmount == 1 {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeeAmount) cups")
-                        }
-                    }
-                }
-                .navigationBarTitle("BetterRest")
-                .navigationBarItems(trailing:
-                    Button(action: calculateBedtime) {
-                        Text("Calculate")
-                    }
-                )
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-                }
-            }
-        }
-    }
-    
-    func calculateBedtime() {
+    var calculateBedtime: String {
         let model = SleepCalculator()
         
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
@@ -84,15 +70,11 @@ struct ContentView: View {
             
             let formatter = DateFormatter()
             formatter.timeStyle = .short
-
-            alertMessage = formatter.string(from: sleepTime)
-            alertTitle = "Your ideal bedtime is…"
+            
+            return formatter.string(from: sleepTime)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Sorry, there was a problem calculating your bedtime."
         }
-        
-        showingAlert = true
     }
 }
 
